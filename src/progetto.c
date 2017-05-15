b087fafd94779787276da96a7c28565f1fce3604
simoberny <simoberny@gmail.com>
2017-05-15 23:17:57 +0200

    File principali aggiornati con semplice codice per testare la funzione build del makefile

---
+++ src/progetto.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "progetto.h"
+
+int main(){
+	func(5);	
+	printf("Ciao Mondo!");
+	return 0;
+}
---
+++ src/progetto.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void func(int var){
+	printf("Libreria stampa %d", var);
+}
