b6f264a3c8192632f88df7c4f6897585c333336c
simoberny <simoberny@gmail.com>
2017-05-16 10:01:04 +0200

    Ciclo base

--- src/progetto.c
+++ src/progetto.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "progetto.h"
 
-int main(){
-	func(5);	
-	printf("Ciao Mondo!");
+int main(int argc, char *argv[]){
+	char comando[20];
+	pid_t pid_tens[7];
+	pid_t pid_units[7];
+	int secondi = 0;
+
+
+	do{
+		printf("Comandi disponibili: \n-start <secondi>: Avvio countdown\n-elapsed: Secondi rimasti\n-stop: Ferma il conto alla rovescia\n-tens: Decine\n-units: Unità\n-quit\n");
+		printf("-----------------\n Run \n-----------------\n");
+		scanf("%s", comando);
+
+		if(strcmp(comando, "start")){
+			printf("Conto alla rovescia di %d iniziato!", secondi);
+		}
+	}while(strcmp(comando, "quit"));
 	return 0;
 }
