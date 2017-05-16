067d2e2b6273cf92527f6bad2f0182b20c56e6b9
simoberny <simoberny@gmail.com>
2017-05-16 10:38:00 +0200

    Aggiornata con fgets lettura comandi + secondi conto alla rovescia

--- src/progetto.c
+++ src/progetto.c
@@ -7,17 +7,26 @@ int main(int argc, char *argv[]){
 	char comando[20];
 	pid_t pid_tens[7];
 	pid_t pid_units[7];
-	int secondi = 0;
+	int secondi = -1;
 
 
 	do{
-		printf("Comandi disponibili: \n-start <secondi>: Avvio countdown\n-elapsed: Secondi rimasti\n-stop: Ferma il conto alla rovescia\n-tens: Decine\n-units: Unità\n-quit\n");
+		printf("\nComandi disponibili: \n-start <secondi>: Avvio countdown\n-elapsed: Secondi rimasti\n-stop: Ferma il conto alla rovescia\n-tens: Decine\n-units: Unità\n-quit\n");
 		printf("-----------------\n Run \n-----------------\n");
-		scanf("%s", comando);
-
-		if(strcmp(comando, "start")){
-			printf("Conto alla rovescia di %d iniziato!", secondi);
+		//scanf("%s%d", comando, &secondi);
+		if ( fgets (comando , 20 , stdin) != NULL ){
+			sscanf(comando, "start %d", &secondi);
+			if(secondi != -1){
+				sscanf(comando, "%s ", comando);
+			}
 		}
-	}while(strcmp(comando, "quit"));
+		
+
+		if(strcmp(comando, "start") == 0 && secondi != -1){
+			printf("Conto alla rovescia di %d iniziato!", secondi);
+		}else if(strcmp(comando, "quit") != 0 ){
+			perror("Manca parametro o comando inesistente");
+		}
+	}while(strcmp(comando, "quit") == 0);
 	return 0;
 }
