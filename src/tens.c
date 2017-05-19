1796ee2097bd8d6fafb05f586b9b5d247aa8c2cc
Bernabe Simone (ACC0107439) <simone.bernabe@sci-austud03x.unitn.it>
2017-05-19 16:39:19 +0200

    usaibdiwq

--- src/tens.c
+++ src/tens.c
@@ -5,7 +5,7 @@
 #include <sys/stat.h> /* For S_IFIFO */
 #include <fcntl.h>
 
-int decine = 0;
+int decine = -1;
 
 int readLine(int fd, char *str){
 	int n; 
@@ -16,7 +16,30 @@ int readLine(int fd, char *str){
 	return n>0;
 }
 
+void countHandler (int sig) {
+	
+	decine--;
+	printf("\nDecremento decine: %d\n", decine);	
+}
+
+int getExPid(char* process){
+	char comand[29];
+	sprintf(comand, "pidof -s %s", process);
+
+	FILE *ls = popen(comand, "r");
+	char buf[256];
+	while (fgets(buf, sizeof(buf), ls) != 0) {
+   	 	//printf("\n PID ( %s ) : %s", process, buf);
+	}
+	pclose(ls);
+	return buf;
+}
+
+
 int main(){
+	void countHandler (int);
+	signal (17, countHandler);
+
 	int fd_tens_in;
 	int fd_tens_out;
 
@@ -30,17 +53,23 @@ int main(){
 	unlink("tens_pipe_out");
 	mknod ("tens_pipe_out", S_IFIFO, 0);
 	chmod ("tens_pipe_out", 0660); 
-	fd_tens_in = open ("tens_pipe_out", O_RDONLY);
+	fd_tens_in = open ("tens_pipe_out", O_NONBLOCK);
+	
+	while(1){
+		if (readLine (fd_tens_in, str)) {
+			sprintf(message, "%s", str);
 
-	while (readLine (fd_tens_in, str)) {
-		sprintf(message, "%s", str);
+			if(strncmp(message, "tens", 4) == 0){
+				sscanf(message, "tens %d", &decine);
+				write (fd_tens_out, message, strlen(message) + 1);
+			}else if(strcmp(message, "elapsed") == 0){
+				sprintf(decine_str, "%d", decine);
+				write (fd_tens_out, decine_str, strlen(decine_str) + 1);
+			}
+		}
 
-		if(strncmp(message, "tens", 4) == 0){
-			sscanf(message, "tens %d", &decine);
-			write (fd_tens_out, message, strlen(message) + 1);
-		}else if(strcmp(message, "elapsed") == 0){
-			sprintf(decine_str, "%d", decine);
-			write (fd_tens_out, decine_str, strlen(decine_str) + 1);
+		if(decine == 0){
+			kill(getExPid("units"), 18);
 		}
 	}
 
--- src/units.c
+++ src/units.c
@@ -6,19 +6,8 @@
 #include <fcntl.h>
 #include <time.h>
 
-int unita = 0;
-
-void countdown(){
-	while(unita >= 0){
-		if(unita == 0){
-			unita = 9;	
-			// INVIO SEGNALE	
-		}
-		sleep(1);
-		unita--;
-	}
-
-}
+int unita = -1;
+int decine = 1;
 
 int readLine(int fd, char *str){
 	int n; 
@@ -29,10 +18,32 @@ int readLine(int fd, char *str){
 	return n>0;
 }
 
+void countHandler (int sig) {
+	unita = 0;
+	decine = 0;
+}
+
+int getExPid(char* process){
+	char comand[29];
+	sprintf(comand, "pidof -s %s", process);
+
+	FILE *ls = popen(comand, "r");
+	char buf[256];
+	while (fgets(buf, sizeof(buf), ls) != 0) {
+   	 	//printf("\n PID ( %s ) : %s", process, buf);
+	}
+	pclose(ls);
+	return buf;
+}
+
 int main(){
+	void countHandler (int);
+	signal (18, countHandler);
+
 	int fd_units_in;
 	int fd_units_out;
-
+	clock_t start, end;
+	int prevdiff = 0;
 	char unita_str[10];
 
 	char str[100];
@@ -45,21 +56,43 @@ int main(){
 	unlink("units_pipe_out");
 	mknod ("units_pipe_out", S_IFIFO, 0);
 	chmod ("units_pipe_out", 0660); 
-	fd_units_in = open ("units_pipe_out", O_RDONLY);
+	fd_units_in = open ("units_pipe_out", O_NONBLOCK);
 
-	while (readLine (fd_units_in, str)) {
-		sprintf(message, "%s", str);
+	while(1){
+		
+		if (readLine (fd_units_in, str)) {
+			sprintf(message, "%s", str);
 			
-		if(strncmp(message, "units", 5) == 0){
-			sscanf(message, "units %d", &unita);
-			write (fd_units_out, message, strlen(message) + 1);
-			countdown();
-		}else if(strcmp(message, "elapsed") == 0){
-			sprintf(unita_str, "%d", unita);
-			write (fd_units_out, unita_str, strlen(unita_str) + 1);
+			if(strncmp(message, "units", 5) == 0){
+				sscanf(message, "units %d", &unita);
+				write (fd_units_out, message, strlen(message) + 1);
+				start = clock();
+			}else if(strcmp(message, "elapsed") == 0){
+				sprintf(unita_str, "%d", unita);
+				write (fd_units_out, unita_str, strlen(unita_str) + 1);
+			}
 		}
+		if(decine > 0){
+
+			if(unita == 0){
+				kill(getExPid("tens"), 17);
+				unita = 10;
+			}
+
+			if(unita > 0){
+				end = clock();
+				int diff = ((end - start)/CLOCKS_PER_SEC);
+				if((diff - prevdiff) >= 1){
+					unita = unita - (diff - prevdiff);
+					printf("Unità: %d\n", unita);
+					prevdiff = diff;
+				}
+			}
+		}
+		
 
 	}
+
 
 	close(fd_units_in);
 	close (fd_units_out);
