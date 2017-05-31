db6a6f375b8179ed779eb19f7278d88b4ec68e40
simoberny <simoberny@gmail.com>
2017-05-31 14:23:37 +0200

    Aggiornati commenti

--- src/progetto.c
+++ src/progetto.c
@@ -13,45 +13,55 @@
 #endif  
 
 //Dichiarazioni pipe
-
 int fd_tens_in;
 int fd_tens_out;
 
 int fd_units_in;
 int fd_units_out;
 
+//Vettore con la funzione di tradurre un numero nel suo corrispettivo a 7 segmenti
 const int segmenti[10][7]={{1,1,1,1,1,1,0},{0,1,1,0,0,0,0},{1,1,0,1,1,0,1},{1,1,1,1,0,0,1},{0,1,1,0,0,1,1},{1,0,1,1,0,1,1},{1,0,1,1,1,1,1},{1,1,1,0,0,0,0,},{1,1,1,1,1,1,1},{1,1,1,1,0,1,1}};  //matrice che mappa ogni  segmento(riga) con ogni numero(colonna)
-const int gpioTens[7]={3,12,30,14,13,0,2}; //mappa i pin gpio con i segmenti
-const int gpioUnits[7]={16,1,21,23,25,15,22}; //mappa i pin gpio con i segmenti
 
-/*   Codice per generazione 7 segmenti a schermo    */
+const int gpioTens[7]={3,12,30,14,13,0,2}; //mappa i pin della libreria wiring con i segmenti per le decine
+const int gpioUnits[7]={16,1,21,23,25,15,22}; //mappa i pin della libreria wiring con i segmenti per le unità
 
+/* Codice per generazione 7 segmenti nella bash (Stack Overflow) */
 #define C(x) ((_[*n-'0'])>>s)&m&x
 #define P putchar
 
-unsigned int _[]={476,144,372,436,184,428,492,148,508,188};
+unsigned _[]={476,144,372,436,184,428,492,148,508,188};
 
 
 void p(int a,char*n,unsigned m,int s){
 	for(;isdigit(*n);++n){
 		P(C(1)?'|':' ');
+
 		for(int i=0;i<a;++i){
 			P(C(4)?'_':' ');
 		}
+
 		P(C(2)?'|':' ');
 	}
+
 	P('\n');
 }
 void l(int a,char*b){
-	p(a,b,7,0);int i=1;
-	for(;i<a;++i)p(a,b,3,3);p(a,b,7,3);i=1;
-	for(;i<a;++i)p(a,b,3,6);p(a,b,7,6);
+	p(a,b,7,0);
+	int i=1;
+	for(;i<a;++i){
+		p(a,b,3,3);
+		p(a,b,7,3);
+		i=1;
+	}
+	for(;i<a;++i){
+		p(a,b,3,6);
+		p(a,b,7,6);
+	}
 }
-
 /* __________________________________________________*/
 
+//Inizializza tutti i pin della gpio come uscite e setta il loro valore a alto(7 Segmenti a catodo comune)
 void gpioInita(){
-	//gpio init	
 	#if (defined TARGET)
 		for(int i=0; i<7; i++){
 			wiringPiSetup () ;
@@ -63,7 +73,7 @@ void gpioInita(){
 	#endif  
 }
 
-
+//Crea le pipe in ingresso (Ricevono dati da tens e units)
 void creazionePipe(){
 	//PIPE DI LETTURA DECINE
 	unlink("tens_pipe_in");
@@ -88,6 +98,7 @@ void closePipe(){
 	unlink("units_pipe_in");
 }
 
+//Legge stringhe in ingresso, carattere per carattere, dato il file descriptor della pipe
 int readLine(int fd, char *str){
 	int n; 
 	do{
@@ -97,26 +108,27 @@ int readLine(int fd, char *str){
 	return n>0;
 }
 
-//Funzione che dato un nome restituisce un PID
+//Funzione che dato il nome di un processo restituisce un PID
 int getExPid(char* process){
 	char comand[29];
 	sprintf(comand, "pidof -s %s", process);
 
-	FILE *ls = popen(comand, "r");
+	FILE *ls = popen(comand, "r"); //A differenza di system restituisce la risposta del terminale
 	char buf[256];
-	while (fgets(buf, sizeof(buf), ls) != 0) {
-	}
+	while (fgets(buf, sizeof(buf), ls) != 0) {}
+
 	pclose(ls);
 	return atoi(buf);
 }
 
-
+//Funzione che inizializza il conto alla rovescia
 void start(int sec){
 	char strin_tens[100];
 	char strin_units[100];	
 	char strout_tens[100];
 	char strout_units[100];
 
+	//Scomposizione secondi in UNITA e DECINE
 	int unita = sec%10;
 	int decine = (sec)/10;
 
@@ -125,6 +137,7 @@ void start(int sec){
 	sprintf(strout_tens, "tens %d", decine);
 	sprintf(strout_units, "units %d", unita);
 
+	//Esecuzione figli indipendenti
 	system("./tens &");
 	system("./units &");
 
@@ -137,13 +150,17 @@ void start(int sec){
 		fd_units_out = open ("units_pipe_out", O_WRONLY);
 	} while (fd_units_out == -1);
 
+	//Invia unità e decine ai rispettivi processi per iniziare il conto alla rovescia
 	write (fd_tens_out, strout_tens, strlen(strout_tens) + 1);
 	write (fd_units_out, strout_units, strlen(strout_units) + 1);
 }
 
+//Funzione che interroga unità e decine sul loro stato e restituisce il tempo rimanente
 void elapsed(){
 	char decine[10];
 	char unita[10];
+
+	//Se i processi sono ancora in esecuzione vengono richiesti i valori
 
 	if(getExPid("tens") != 0){
 		write (fd_tens_out, "elapsed", strlen("elapsed") + 1);
@@ -164,12 +181,15 @@ void elapsed(){
 		strcpy(unita, "0");
 	}
 
+	//printf("Tempo rimanente: %s%s\n", decine, unita);
 
+	//Stampa più elaborata con rappresentazione a 7 segmenti
 	char numero[10];
 	sprintf(numero, "%s%s", decine, unita);
 	l(3, numero);
 }
 
+//Ferma il timer inviando un comando ai due processi
 void stop(){
 	if(getExPid("tens") != 0){
 		write (fd_tens_out, "stop", strlen("stop") + 1);
@@ -194,6 +214,7 @@ void printUnits(){
 	}
 }
 
+//Dato il segmento restituisce il suo stato
 void getTens(int led){
 	if(led >= 0 && led < 7){
 		if(getExPid("tens") != 0){
@@ -206,6 +227,7 @@ void getTens(int led){
 	}
 }
 
+//Dato il segmento e un colore setta il colore di quel segmento
 void setTens(int led, char * color){
 	if(led >= 0 && led < 7){
 		if(getExPid("tens") != 0){
@@ -218,6 +240,7 @@ void setTens(int led, char * color){
 	}
 }
 
+//Dato il segmento restituisce il suo stato
 void getUnits(int led){
 	if(led >= 0 && led < 7){
 		if(getExPid("units") != 0){
@@ -230,6 +253,7 @@ void getUnits(int led){
 	}
 }
 
+//Dato il segmento e un colore setta il colore di quel segmento
 void setUnits(int led, char * color){
 	if(led >= 0 && led < 7){
 		if(getExPid("units") != 0){
@@ -247,39 +271,38 @@ void pipeHandler(int sig){
 }
 
 
-int main(int argc, char *argv[]){
-	//system("killall -s SIGKILL units 2>&1");
-	//system("killall -s SIGKILL tens 2>&1");
-	
+int main(int argc, char *argv[]){	
 	void pipeHandler (int);
 	signal (SIGPIPE, pipeHandler);
 	
 	gpioInita();
 
 	char comando[100];
+	char directory[100];
+	char ledcolor[50];
 	int secondi = -1;
 	int led;
-	char ledcolor[50];
-
+	int secimp = 0;
+	
 	FILE *fd;
-	char directory[100];
-
+	
 	creazionePipe();
 
-	int secimp = 0;
-
+	//Se al programma vengono passati i secondi attraverso la console, questi vengono salvati in un file
 	if(argc > 1){
 		secimp = atoi(argv[1]);
 		if(secimp > -1 && secimp < 60){
 			fd=fopen("../assets/testmode.txt", "ab+");
 			fprintf(fd, "%d", secimp);
 			fclose(fd);
+
+			//Conto alla rovescia avviato con i secondi presi dai parametri in ingresso
 			start(secimp);
 		}
 	}
 
 	do{	
-		
+		//Piccolo ritardo per visualizzare il risultato dei figli prima del menu dinamico
 		usleep(200000);
 		printf("\n	Comandi disponibili: \n	-start <secondi>: Avvio countdown\n	-elapsed: Secondi rimasti\n	-stop: Ferma il conto alla rovescia\n	-tens: Decine\n	-units: Unità\n	-tensled info <n>\n	-unitsled info <n>\n	-tensled color <n> <color>\n	-unitsled color <n> <color>\n	-quit\n");
 		printf("-----------------\n Comando > ");
@@ -320,6 +343,7 @@ int main(int argc, char *argv[]){
 			}
 		}
 		
+		//Ciclo console dinamica eseguita fino alla ricezione del comando "quit"
 	}while(!(strcmp(comando, "quit") == 0));
 
 	closePipe();
--- src/tens.c
+++ src/tens.c
@@ -7,7 +7,9 @@
 #include <sys/unistd.h>
 #include <signal.h>
 
+//Se la dafinizione TARGET esiste, creata se il make rileva un sistema Raspberry, include la libreria per raspberry
 #if (defined TARGET)
+	//Libreria utilizzata da Raspberry
 	#include <wiringPi.h>
 #endif  
 
@@ -21,10 +23,11 @@ int fd_tens_out;
 pid_t pidFiglio[7];
 int fds[7][2];
 
+//Vettore con la funzione di tradurre un numero nel suo corrispettivo a 7 segmenti
 const int segmenti[10][7]={{1,1,1,1,1,1,0},{0,1,1,0,0,0,0},{1,1,0,1,1,0,1},{1,1,1,1,0,0,1},{0,1,1,0,0,1,1},{1,0,1,1,0,1,1},{1,0,1,1,1,1,1},{1,1,1,0,0,0,0,},{1,1,1,1,1,1,1},{1,1,1,1,0,1,1}};  //matrice che mappa ogni  segmento(riga) con ogni numero(colonna)
-const int gpioTens[7]={3,12,30,14,13,0,2}; //mappa i pin gpio con i segmenti
+const int gpioTens[7]={3,12,30,14,13,0,2}; //mappa i pin della libreria wiring con i segmenti per le decine
 
-
+//Funzione che chiude tutte le pipe, elimina tutti i processi figli relativi ai segmenti e termina il processo corrente
 void closeAll(){
 	for(int i = 0; i < 7; i++){
 		kill(pidFiglio[i], SIGKILL);
@@ -35,6 +38,7 @@ void closeAll(){
 	exit(0);
 }
 
+//Funzione che dato il nome di un processo restituisce un PID
 int getExPid(char* process){
 	char comand[29];
 	sprintf(comand, "pidof -s %s", process);
@@ -48,21 +52,25 @@ int getExPid(char* process){
 	return atoi(buf);
 }
 
+//Handler che viene chiamato quando arriva il segnale inviato dal processo units che termina 
 void killHandler(int sig){
 	closeAll();
 }
 
+//Handler che viene chiamato quando il processo units arriva a 0 e invia il segnale alla decine affinchè si decrementino 
 void countHandler (int sig) {
+	//Se ci sono ancora decine vengono decrementate
 	if(decine > 0){
 		decine--;
-		//printf("\nDecremento decine: %d\n", decine);
 	}
 
+	//Aggiornamento dei singoli segmenti inviando le decine al processi figli
 	for(int i = 0; i < 7; i++){		
 		char m[100];	
 		sprintf(m, "n %d non non", decine);
 		write(fds[i][WRITE], m, strlen(m) + 1);		
 
+		//Aggiornamento 7 segmenti sempre se la definizione Target esiste
 		#if (defined TARGET)
 			//gipo decremento!
 			wiringPiSetup () ;
@@ -72,26 +80,36 @@ void countHandler (int sig) {
 
   	}
 
+  	//Se le decine arrivano a 0 il processo invia un segnale alle unità per avvertirle che il successivo è l'ultimo conteggio che devono eseguire
 	if(decine == 0){
 		kill(getExPid("units"), SIGUSR2);		
 	}
 }
 
+//Funzione che crea i 7 figli per gestire i singoli segmenti
 void creazioneFigli(char ** argv){
 
 	int pid;
+
 	for(int i = 0; i<7; i++){
+
+		//Creazione pipe anonima per ogni figlio
 		pipe(fds[i]);
 		pid = fork();
+
+		//Salvataggio dei pid dei figli
 		pidFiglio[i] = pid;
 
 		if (pid==0){
+			//Processo figlio numero #i
+
 			unlink("tens_pipe_out");
 			close(fd_tens_in);
 			close (fd_tens_out);
 
 			FILE *fd;
 
+			//Viene cambiato il nome dei figli
 			int argv0size = strlen(argv[0]);
 			strncpy(argv[0], "figliotens", argv0size);
 
@@ -99,41 +117,51 @@ void creazioneFigli(char ** argv){
 			char comando[100];
 			char tmpColor[50];
 
+			//Variabile per salvare lo stato del segmento: "OFF se spento", "#COLORE se acceso"
 			char stato[50];
+
+			//Variabile che salva il colore del segmento indipendentemente dallo stato
 			char colore[50];
+
+			//Valori di default dei segmenti
 			strcpy(stato, "off");
 			strcpy(colore, "red");
 
+			//Ciclo dei figli
 			while (1){							
-				
 				int bytesRead;
+				//Legge stringhe in arrivo sulla pipe
 				while((bytesRead = read(fds[i][READ], messag, 100)) > 0){
 					int valore=0;
 					int led = 0;
 
 					if(strncmp(messag, "n", 1) == 0){
+						//Salvo il valore delle decine attuali, il comando richiesto se richiesto e un eventuale colore da settare
 						sscanf(messag, "n %d %s %s", &valore, comando, tmpColor);
+
+						//Controllo se questo segmento (Figlio #i) è acceso dato il numero attuale
 						if(segmenti[valore][i] == 1){
 							strcpy(stato, colore);
 						}else{
 							strcpy(stato, "off");
-						}
-						
+						}		
 
+						//Salvo lo stato del segmento su file, nella cartella assets
 						char directory[100];
 						sprintf(directory, "../assets/tens_led_%d", i);
 											
-						
 						fd=fopen(directory, "w");
 
 						if(fd != NULL){
 							fprintf(fd, "%s\n", stato);
   							fclose(fd);
 						}
-
+				
 						if(strcmp(comando, "Info") == 0){
+							//Richiesta stato segmento
 							printf("Stato LED Decine%d: %s \n",i, stato);
 						}else if(strcmp(comando, "Color") == 0){
+							//Setto il colore di un dato segmento
 							strcpy(colore, tmpColor);
 							printf("Colore settato: %s\n", colore);
 						}						
@@ -154,6 +182,7 @@ void creazioneFigli(char ** argv){
 	signal (SIGKILL, killHandler);
 }
 
+//Legge stringhe in ingresso, carattere per carattere, dato il file descriptor della pipe
 int readLine(int fd, char *str){
 	int n; 
 	
@@ -178,10 +207,12 @@ int main(int argc, char ** argv){
 
 	int led = -1;
 
+	//Apertura pipe di uscita per comunicare con il processo principale
 	do {
 		fd_tens_out = open ("tens_pipe_in", O_WRONLY);
 	} while (fd_tens_out == -1);
 
+	//Creazione pipe
 	unlink("tens_pipe_out");
 	mknod ("tens_pipe_out", S_IFIFO, 0);
 	chmod ("tens_pipe_out", 0660); 
@@ -191,7 +222,11 @@ int main(int argc, char ** argv){
 		sprintf(message, "%s", str);
 		if(strncmp(message, "tens", 4) == 0){
 			sscanf(message, "tens %d", &decine);
+
+			//Vengono creati i 7 figli che gestiscono i segmenti
 			creazioneFigli(argv);
+
+			//Se le decine inserite sono già 0 viene inviato subito il segnale
 			if(decine == 0){
 				kill(getExPid("units"), SIGUSR2);				
 			}
@@ -214,18 +249,21 @@ int main(int argc, char ** argv){
 			sprintf(richiesta, "Color %s", readcolor);
 		}
 
+		//Per ogni segmento viene generata la richiesta da effettuare 
 		for(int i = 0; i < 7; i++){			
 			if(led == i){
 				sprintf(msgPip, "n %d %s", decine, richiesta);
 				led = -1;
 			}else{
-				sprintf(msgPip, "n %d non non", decine);
+				//Se non c'è nessuna richiesta vengono semplicemente inviate le decine attuali
+				//Al posto della richiesta vengono inseriti due semplici "-" per skippare la richiesta
+				sprintf(msgPip, "n %d - -", decine);
 			}
 			write(fds[i][WRITE], msgPip, strlen(msgPip) + 1);
 			
+			//Settaggio iniziale dei 7 segmenti
 			#if (defined TARGET)
-				//gipo !
-				wiringPiSetup () ;
+				wiringPiSetup();
 				pinMode (gpioTens[i], OUTPUT);
 				digitalWrite (gpioTens[i], !segmenti[decine][i]);
 			#endif
